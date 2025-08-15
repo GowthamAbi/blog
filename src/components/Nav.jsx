442e3c2b7ce378958c144dbb25e5caf9c37813b5
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-08-15 13:23:30 +0530

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,8 @@ export default function Nav() {
     <>
    <nav className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 '>
     <img src={assets.logo} alt="logo" className='w-32 sm:w-44'/>
-    <button >Login <img src={assets.arrow} alt="arrow"  className='w-3 '/></button>
+    <button className='flex items-center text-white px-10 py-2.5 gap-2 rounded-full text-dm cursor-pointer bg-primary sm:mx-20 xl:mx-32' >Login
+       <img src={assets.arrow} alt="arrow"  className='w-3 '/></button>
 
    </nav>
     </>
