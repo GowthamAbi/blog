9de4306bf363d25cb813d1fd081f1a603cceb5b8
GowthamAbi <gowtham2131ece@gmail.com>
2025-08-18 18:45:45 +0530

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import AdminPage from './pages/AdminPage'
+
+
 
 function App() {
 
@@ -11,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/blog' element={<Blog/>}/>
+        <Route path='/adminlogin' element={<AdminPage/>}/>
       </Routes>
     </>
   )
--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -7,7 +7,7 @@ export const NewsLetter = () => {
             <h1 className='font-bold text-4xl pb-2'>Never Miss a Blog!</h1>
             <h5 className='text-gray-500'>Subscribe to get the latest blog, new tech, and exclusive news.</h5>
             <form className='flex border border-gray-400 mx-auto max-w-lg bg-white overflow-hidden max-sm:scale-75 h-12  rounded-sm mt-8  ' >
-                <input type="text" placeholder='Enter Your Email' className='w-full pl-2' required="true"/>
+                <input type="text" placeholder='Enter Your Email' className='w-full pl-2' />
                 <button className='bg-primary text-white text-sm p-2 rounded-esm'>Subscribe</button>
             </form>
         </div>
