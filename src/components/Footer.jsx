b46150c22c30dacfbb1f2d8bd90f1351fa99dbfb
GowthamAbi <gowtham2131ece@gmail.com>
2025-08-18 18:08:40 +0530

    Home Page Complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import { assets,footer_data } from '../assets/assets'
-import { div, h1 } from 'motion/react-client'
+import { div, h1, li } from 'motion/react-client'
 
 export const Footer = () => {
   return (
     <div className='bg-gray-200 mt-60 p-4'>
         <div className='mx-8 sm:mx-20 xl:mx-32 flex justify-between'>
-            <div className='w-60'>
+            <div className=' '>
                 <img src={assets.logo} />
-                <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum repellat
+                <p className='text-gray-500 max-w-[410px] mt-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum repellat
                      quibusdam ad enim necessitatibus impedit amet, officiis, nesciunt,
                     tenetur distinctio vel?
                      Architecto rerum minus nihil ipsum sit velit nam. Exercitationem?</p>
             </div>
-            <div className='flex justify-between '>
+            <div className='flex flex-wrap justify-between w-full md:w-[45%] '>
                     {footer_data.map((item,index)=>(
-                        <div key={index} className='w-40 flex-wrap'>
-                        <h1>{item.title}</h1>
-                        <h1>{item.links}</h1>
+                        <div key={index} className=' w-40 '>
+                        <h3 className='mb-8 font-semibold'>{item.title}</h3>
+                        <ul>
+                          {item.links.map((link,i)=>(
+                            <li key={i} className='text-gray-500'><a href="#">{link}</a></li>
+                          ))}
+                        </ul>
                         </div>
                     ))}
             </div>
+            
+            </div>
+            <div className='mx-8 sm:mx-20 xl:mx-32 mt-8 text-gray-500 text-center '>
+              <hr  />
+              <p className='pt-4'>Copyright 2025 © QuickBlog GreatStack - All Right Reserved.</p>
             </div>
         
     </div>
--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const NewsLetter = () => {
   return (
     <div>
-        <div className='flex-col justify-center  text-center'>
-            <h1 className='font-bold text-4xl'>Never Miss a Blog!</h1>
+        <div className='flex-col justify-center  text-center '>
+            <h1 className='font-bold text-4xl pb-2'>Never Miss a Blog!</h1>
             <h5 className='text-gray-500'>Subscribe to get the latest blog, new tech, and exclusive news.</h5>
-            <form className='flex border border-gray-400 mx-auto max-w-lg bg-white overflow-hidden max-sm:scale-75 h-12  rounded-sm  ' >
+            <form className='flex border border-gray-400 mx-auto max-w-lg bg-white overflow-hidden max-sm:scale-75 h-12  rounded-sm mt-8  ' >
                 <input type="text" placeholder='Enter Your Email' className='w-full pl-2' required="true"/>
                 <button className='bg-primary text-white text-sm p-2 rounded-esm'>Subscribe</button>
             </form>
