bd2acfe4b9fb52ee5dca6e4bd226a0bee60aefc1
GowthamAbi <gowtham2131ece@gmail.com>
2025-08-21 17:12:06 +0530

    Blogs

--- src/pages/Options/BlogList.jsx
+++ src/pages/Options/BlogList.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TableData from '../../components/Data/TableData'
+import { assets } from '../../assets/assets'
+import { tr } from 'motion/react-client'
 
 export default function BlogList() {
+
+  const[data,setData]=useState('')
+const fetchBlogs=async()=>{
+  setData(assets.blog_data)
+}
+
+useEffect(()=>{
+  fetchBlogs()
+},[])
+
   return (
-    <div>BlogList</div>
+    <div className='bg-blue-300/10 w-full'>
+      <div className='mt-12 ml-12 '>
+        <h1>All blogs</h1>
+        <div className='bg-white max-w-2xl  mt-4 rounded-tl-lg shadow border-b border-gray-500 scroll-auto '>
+          <table className='w-full'>
+            <thead className='uppercase'>
+              <tr >
+                <td scope='col' className='px-2 py-4'>#</td>
+                <td scope='col'  className='px-2 py-4'>Blog Title</td>
+                <td scope='col' className='px-2 py-4'>Date</td>
+                <td scope='col' className='px-2 py-4'>Status</td>
+                <td scope='col' className='px-2 py-4'>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+
+              {
+                data.map((blog,index)=>{
+                  <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{data.title}</td>
+                  </tr>
+
+                })
+              }
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
